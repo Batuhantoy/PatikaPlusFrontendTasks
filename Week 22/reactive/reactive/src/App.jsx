4a18411f7a31ef952480add6d68e28300f1c2d30
Batuhan <980.batuhantoy@gmail.com>
2024-02-19 23:11:36 +0300

    Week 22

---
+++ Week 22/reactive/reactive/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Section1 from '/section1/section1'
+import Section2 from '/section1/section2'
+import Section3 from '/section1/section3'
+import Section4 from '/section4'
+import Section5 from '/section5'
+import Section6 from '/section6/section6'
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <Section1></Section1>
+      <Section2></Section2>
+      <Section3></Section3>
+      <Section4></Section4>
+      <Section5></Section5>
+      <Section6></Section6>
+    </div>
+  )
+}
+
+export default App
